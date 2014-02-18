fd86cfb40894648e889d099eb6c18e0bb92350f9
nifus <nifus@mail.ru>
2014-02-18 20:02:37 +0400

    ajax ext

--- src/classes/extensions/ajax/Ajax.php
+++ src/classes/extensions/ajax/Ajax.php
@@ -1,28 +1,32 @@
 <?php
+namespace Nifus\FormBuilder\Extensions;
 
-namespace Nifus\FormBuilder;
-
-class AjaxExtension{
+use \Nifus\FormBuilder\Extension as Extension;
+class Ajax extends Extension{
 
     static function form(){
         //dd($_POST);
         return true;
     }
 
-    static function autoload($formBuilder){
-        FormBuilder::jsAdd('jquery');
-        FormBuilder::jsAdd('jquery.form','ajax');
-        $config = $formBuilder->config;
+    public function loadAsset(){
+
+        Render::jsAdd('jquery');
+        Render::jsAdd('jquery.form','ajax');
+
+        $config = $this->render->config;
         if ( isset($config['ajax']) && is_array($config['ajax']) ){
             //dd( \View::make('formbuilder::extends/ajax/js')->render() );
             $v = \View::make('formbuilder::extends/ajax/js')
                 ->with('formName',$config['id'])
                 ->with('formAction',$config['ajax']['url']);
-            $formBuilder->setJs(  $v->render(),$v->getPath() );
+            $this->render->setJs(  $v->render(),$v->getPath() );
         }
+
     }
 
-    static function fields(){
+
+    public function configField($config){
         return [];
     }
 }
