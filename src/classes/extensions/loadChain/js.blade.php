13195cf53a8eb074e2926c206f665988adf8465f
nifus <nifus@mail.ru>
2014-05-29 07:49:37 +0400

    refactoring chain

--- src/classes/extensions/loadChain/js.blade.php
+++ src/classes/extensions/loadChain/js.blade.php
@@ -1,42 +1,51 @@
 <script type="text/javascript">
     var Chain={
         init:function(form){
-            this.form = $('#'+form);
-            this.source = $('#'+form).find('*[data-source]');
-            this.receiver = $('#'+form).find('*[name='+this.source.attr('data-receiver')+']');
+            var form = $('#'+form);
 
-            $('body').on('change','#'+this.source.attr('id'),function()
+            var source = form.find('*[data-source]');
+
+            source.on('change',function()
             {
-
-                Chain.receiver.attr('disabled','disabled');
-                var id = Chain.source.val();
-                var url = Chain.source.attr('data-url');
+                var id =  $(this).val();
                 if ( id==0 ){
                     return false;
                 }
-                $.ajax({
-                    dataType: Chain.source.attr('data-format'),
-                    type: "post",
-                    url: url,
-                    data: { id: id }
-                }).done(function( data ) {
-                        var select = '';
-                        for (var i = 0; i < data.length; i++) {
-                            select += '<option value="' + data[i].city_id + '">' +
-                                data[i].title + '</option>';
-                        }
-                        Chain.receiver.html(select);
-                        Chain.receiver.removeAttr('disabled');
-                });
+                var url =  $(this).attr('data-url');
+                var receiver_id= $(this).attr('data-receiver');
+                var type = $(this).attr('data-format')
+                var  receiver = form.find('*[name='+receiver_id+']');
+                Chain.ajax(type,id,receiver,url );
+            });
 
+
+            source.each( function(){
+                $(this).trigger('change');
             })
-            this.source.trigger('change');
+        },
 
+        ajax:function(type,id,receiver,url){
+            var receiver = receiver;
+            receiver.attr('disabled','disabled');
+
+            $.ajax({
+                dataType: type,
+                type: "post",
+                url: url,
+                data: { id: id }
+            }).done(function( data ) {
+                    var select = '';
+                    for (var i = 0; i < data.length; i++) {
+                        select += '<option value="' + data[i].city_id + '">' +
+                            data[i].title + '</option>';
+                    }
+                    receiver.html(select).removeAttr('disabled').trigger('change');
+            });
         }
-    }
+    };
+
     $(document).ready(function() {
         Chain.init('{{$id_form}}');
-
     });
 </script>
 
