eed307d60c01fb4800f69cea37e4d94f3f15d5bd
nifus <nifus@mail.ru>
2014-05-29 10:34:11 +0400

    fix loadchain

--- src/classes/extensions/loadChain/js.blade.php
+++ src/classes/extensions/loadChain/js.blade.php
@@ -2,25 +2,30 @@
     var Chain={
         init:function(form){
             var form = $('#'+form);
+            form.find('*[data-receiver]').each(function(){
+                var el = $(this).attr('data-receiver');
+                form.find('*[name='+el+']').attr('disabled','disabled');
+                //console.log( form.find('*[name='+el+']').length );
+            });
 
             var source = form.find('*[data-source]');
-
             source.on('change',function()
             {
                 var id =  $(this).val();
-                if ( id==0 ){
-                    return false;
+                if ( id>0 ){
+                    var url =  $(this).attr('data-url');
+                    var receiver_id= $(this).attr('data-receiver');
+                    var type = $(this).attr('data-format')
+                    var  receiver = form.find('*[name='+receiver_id+']');
+                    Chain.ajax(type,id,receiver,url );
                 }
-                var url =  $(this).attr('data-url');
-                var receiver_id= $(this).attr('data-receiver');
-                var type = $(this).attr('data-format')
-                var  receiver = form.find('*[name='+receiver_id+']');
-                Chain.ajax(type,id,receiver,url );
             });
 
 
             source.each( function(){
-                $(this).trigger('change');
+                if ( $(this).attr('disabled')!='disabled' ){
+                    $(this).trigger('change');
+                }
             })
         },
 
@@ -36,7 +41,7 @@
             }).done(function( data ) {
                     var select = '';
                     for (var i = 0; i < data.length; i++) {
-                        select += '<option value="' + data[i].city_id + '">' +
+                        select += '<option value="' + data[i].id + '">' +
                             data[i].title + '</option>';
                     }
                     receiver.html(select).removeAttr('disabled').trigger('change');
